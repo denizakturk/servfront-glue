0c6004a625fc79879cef87661e807aabd6fb8e99
Deniz Aktürk <deniz.akturk@hotmail.com>
2021-12-20 10:56:15 +0300

    Generator

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go run ./generator ./template/hello.go.tmpl ./helper.go ./schema.json
+//go:generate go run servfront-glue/generator ./example/hello.go.tmpl ./helper.go ./example/param.json
 func main() {
 
 }
